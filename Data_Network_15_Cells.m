1038c961b81e88a8f3302f13f91b95776c0fb431
Zakaria Abdelmoiz Dahi <68249696+Zakaria-Dahi@users.noreply.github.com>
2021-12-29 12:07:52 +0100

    Add files via upload

---
+++ Data_Network_15_Cells.m
@@ -0,0 +1,18 @@
+% Date: 15-12-2021
+% About the code: the data are organised in pairs (Location Update, Paging Cost)
+
+network = [2	1	;
+20	2;
+4	1;
+84	10;
+23	1;
+3	1;
+16	10;
+7	2;
+7	1;
+9	3;
+1	1;
+4	1;
+4	1;
+7	3;
+8	3];
